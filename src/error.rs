18b120368c2943ed73f6813c83af56dd9ec2e1db
phinnl <phinnguyenlong@gmail.com>
2024-03-15 12:59:09 +0700

    first commit

---
+++ src/error.rs
@@ -0,0 +1,31 @@
+use crate::model;
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+	// -- Modules
+	Model(model::Error),
+  ConfigMissingEnv(&'static str),
+}
+
+// region:    --- Froms
+impl From<model::Error> for Error {
+	fn from(val: model::Error) -> Self {
+		Self::Model(val)
+	}
+}
+// endregion: --- Froms
+
+// region:    --- Error Boilerplate
+impl core::fmt::Display for Error {
+	fn fmt(
+		&self,
+		fmt: &mut core::fmt::Formatter,
+	) -> core::result::Result<(), core::fmt::Error> {
+		write!(fmt, "{self:?}")
+	}
+}
+
+impl std::error::Error for Error {}
+// endregion: --- Error Boilerplate
